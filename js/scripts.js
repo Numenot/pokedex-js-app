e8d4c0f0b76be9e751baf786f745048362378754
Numenot <qlv6719@gmail.com>
2021-10-14 16:59:29 +0200

    Added pokemon list array with a few example pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@ alert('Hello world');
 
 let favoriteFood = 'Burger';
 document.write(favoriteFood);
+
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['grass', 'poison'] },
+  { name: 'Charmander', height: 0.6, weight: 8.5, type: ['fire'] },
+  { name: 'Squirtle', height: 0.5, weight: 9, type: ['water'] },
+  { name: 'Pidgey', height: 0.3, weight: 1.8, type: ['flying', 'normal'] }
+];
