793ff04b8ca2b5ab6d55ccff4588ba6f3d079256
Numenot <qlv6719@gmail.com>
2021-10-19 11:58:15 +0200

    Added for loop to list pokemons
    List of pokemons with height, weight and conditional message if large pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
-alert('Hello world');
-
-let favoriteFood = 'Burger';
-document.write(favoriteFood);
-
 let pokemonList = [
-  { name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['grass', 'poison'] },
-  { name: 'Charmander', height: 0.6, weight: 8.5, type: ['fire'] },
-  { name: 'Squirtle', height: 0.5, weight: 9, type: ['water'] },
-  { name: 'Pidgey', height: 0.3, weight: 1.8, type: ['flying', 'normal'] }
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    type: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    weight: 9,
+    type: ['water']
+  },
+  {
+    name: 'Pidgey',
+    height: 0.3,
+    weight: 1.8,
+    type: ['flying', 'normal']
+  },
+  {
+    name: 'Blastoise',
+    height: 1.6,
+    weight: 85.5,
+    type: ['water']
+  }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m , weight: ' + pokemonList[i].weight + ' kg)');
+  if (pokemonList[i].height > 1.5) {
+    document.write(" - Wow that's big!")
+  }
+}
